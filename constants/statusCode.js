b2e7c6e5f361b003d8d41a456a23136730bf606c
DanielDD893 <danieldomzalski080903@gmail.com>
2025-03-25 18:21:30 +0100

    server config status

--- constants/statusCode.js
+++ constants/statusCode.js
@@ -1,3 +1,7 @@
 // 🔧 Zdefiniuj obiekt STATUS_CODE, który przechowuje kody statusu HTTP 302 (FOUND) oraz 404 (NOT_FOUND).
-
+ const STATUS_CODE = {
+    FOUND: 302,       // Przekierowanie
+    NOT_FOUND: 404    // Nie znaleziono
+};
 // 🔧 Wyeksportuj STATUS_CODE, który przechowuje kody statusu HTTP.
+ module.exports = STATUS_CODE;
